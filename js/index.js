66ce9aabdae76b4493dc0996905bda0cfc81b6af
JOSEBRANDL <jbrandlondoo@yahoo.es>
2019-06-11 01:15:50 -0500

    creacion de sopa de letras 1
    las palabras se sobresecriben

--- js/index.js
+++ js/index.js
@@ -3,9 +3,10 @@ window.onload = start;
 var charFill = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
 var boxes = [];
 var words = [];
+var failWord = [];
 //estos valores se utilizan para dimensionar inicialmente la sopa de letras, pero pueden cambiar segun las palabras ingresadas.
-var heigth = 10;
-var width = 10;
+var heigth = 15;
+var width = 15;
 
 function start(){
 	console.log("Bienvenido!");
@@ -33,7 +34,7 @@ function setWords(){
 	let word;
 	for (var i = 0; i < 10; i++) {
 		word = new Object();
-		word.chars = Array.from("hola mundo");
+		word.chars = Array.from("juan");
 		word.size = word.chars.length;
 		word.organization = getRandomInt(1, 9);
 		words.push(word);
@@ -70,6 +71,7 @@ function createBoxes(){
 function putAllWordsInBoxes(){
 	for (var i = 0; i < words.length; i++) {
 		putWordInBox(words[i]);
+		console.log(i);
 	}
 }
 
@@ -79,13 +81,190 @@ function putAllWordsInBoxes(){
 *@return 
 */
 function putWordInBox(word){
-	let displacement = getDisplacement(word.organization);
-	let position = getPositionInit(word);
-	for (var i = 0; i < (heigth*width); i++) {
-		for (var j = 0; j < word.size; j++) {
-			
+	let organization = word.organization;
+	if (organization == 1 || organization == 2) {
+		putHorizontal(word);
+	}
+	if (organization == 3 || organization == 4) {
+		putVertical(word);
+	}
+	if (organization == 5 || organization == 6) {
+		putDiagonalLeft(word);
+	}
+	if (organization == 7 || organization == 8) {
+		putDiagonalRight(word);
+	}
+}
+
+/**
+*@description
+*@param
+*@return 
+*/
+function putVertical(word){
+	let arrayLetter = word.chars;
+	let boxesTemp, column = getRandomInt(0,(width*heigth)-word.size);;
+	let index;
+	if (word.organization == 4) {
+		arrayLetter = arrayLetter.reverse();
+	}
+	while(true){
+		boxesTemp = boxes;
+		for (var i = 0; i < arrayLetter.length; i++) {
+			index = (i*width)+column;
+			if (!boxesTemp[index].state) {
+				boxesTemp[index].nLetter = arrayLetter[i];
+				boxesTemp[index].state = 1; 
+			}else{
+				if (boxesTemp[index].nLetter == arrayLetter[i]) {
+
+				}else{
+					break;
+				}
+			}
+			if (i == (arrayLetter.length-1)) {
+				boxes = boxesTemp;
+				return;
+			}
 		}
-		position+=1;
+		if ((column+arrayLetter.length)>heigth) {
+			failWord.push(word);
+			return;
+		}
+		column++;
+	}
+}
+
+/**
+*@description
+*@param
+*@return 
+*/
+function putHorizontal(word){
+	let arrayLetter = word.chars;
+	let boxesTemp;
+	let row = getRandomInt(0,(width*heigth)-word.size);
+	let index;
+	if (word.organization == 2) {
+		arrayLetter = arrayLetter.reverse();
+	}
+	while(true){
+		boxesTemp = boxes;
+		for (var i = 0; i < arrayLetter.length; i++) {
+			index = row+i;
+			if (!boxesTemp[index].state) {
+				boxesTemp[index].nLetter = arrayLetter[i];
+				boxesTemp[index].state = 1; 
+			}else{
+				if (boxesTemp[index].nLetter == arrayLetter[i]) {
+
+				}else{
+					break;
+				}
+			}
+			if (i == (arrayLetter.length-1)) {
+				boxes = boxesTemp;
+				return;
+			}
+		}
+		if ((row+arrayLetter.length)>width) {
+			row += arrayLetter.length;
+			if (row > (width*heigth)) {
+				failWord.push(word);
+				return;
+			}
+		}else{
+			row++;
+		}
+	}
+}
+
+/**
+*@description
+*@param
+*@return 
+*/
+function putDiagonalLeft(word){
+	let arrayLetter = word.chars;
+	let boxesTemp;
+	let displacement = 0;
+	let index = getRandomInt(0,(width*heigth)-word.size);
+	if (word.organization == 6) {
+		arrayLetter = arrayLetter.reverse();
+	}
+	while(true){
+		boxesTemp = boxes;
+		index = 0;
+		for (var i = 0; i < arrayLetter.length; i++) {
+			if (!boxesTemp[index].state) {
+				boxesTemp[index].nLetter = arrayLetter[i];
+				boxesTemp[index].state = 1; 
+			}else{
+				if (boxesTemp[index].nLetter == arrayLetter[i]) {
+
+				}else{
+					break;
+				}
+			}
+			if (i == (arrayLetter.length-1)) {
+				boxes = boxesTemp;
+				return;
+			}
+			index += width+1+displacement;
+		}
+		if ((displacement+arrayLetter.length)>width) {
+			displacement += arrayLetter.length;
+			if ((displacement+arrayLetter.length)>heigth) {
+				failWord.push(word);
+				return;
+			}
+		}else{
+			displacement++;
+		}
+	}
+}
+
+/**
+*@description
+*@param
+*@return 
+*/
+function putDiagonalRight(word){
+	let arrayLetter = word.chars;
+	let boxesTemp;
+	let displacement = 0;
+	let index = getRandomInt(0,(width*heigth)-word.size);
+	if (word.organization == 8) {
+		arrayLetter = arrayLetter.reverse();
+	}
+	while(true){
+		boxesTemp = boxes;
+		for (var i = 0; i < arrayLetter.length; i++) {
+			if (!boxesTemp[index].state) {
+				boxesTemp[index].nLetter = arrayLetter[i];
+				boxesTemp[index].state = 1; 
+			}else{
+				if (boxesTemp[index].nLetter == arrayLetter[i]) {
+
+				}else{
+					break;
+				}
+			}
+			if (i == (arrayLetter.length-1)) {
+				boxes = boxesTemp;
+				return;
+			}
+			index += width-1+displacement;
+		}
+		if ((displacement+arrayLetter.length)>width) {
+			displacement += arrayLetter.length;
+			if ((displacement+arrayLetter.length)>heigth) {
+				failWord.push(word);
+				return;
+			}
+		}else{
+			displacement++;
+		}
 	}
 }
 
@@ -98,3 +277,23 @@ function putWordInBox(word){
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+
+
+function prueba(){
+	setWords();
+	createBoxes();
+	putAllWordsInBoxes();
+	let soup = "";
+	for (var i = 1; i <= (width*heigth); i++) {
+		if (boxes[i-1].state) {
+			soup += ".";
+		}
+		soup += "|"+boxes[i-1].nLetter;
+		if (i%10 == 0) {
+			console.log(soup);
+			soup = "";
+		}
+	}
+}
+
